c5f5d51bfe8ddacab01a025962454a275a47eed1
Gafour Panolong <96974562+Gafour101@users.noreply.github.com>
2023-07-25 22:35:30 +0800

    test

--- generate.php
+++ generate.php
@@ -1,5 +1,8 @@
 <?php
-
+    
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+// ... rest of the code
     // Include the necessary QR code library
     require_once 'vendor/autoload.php';
 
